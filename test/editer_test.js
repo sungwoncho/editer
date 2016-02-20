03b887ce85f718d8c49c220fd1e3ee774f75ed2f
Sung Won Cho <mikeswcho@gmail.com>
2016-02-21 10:41:29 +1100

    Fix a bug where modifiers can have two linebreaks before or after

--- test/editer_test.js
+++ test/editer_test.js
@@ -19,6 +19,13 @@ describe(".insert", function() {
 
       expect(result).to.equal('line 1\nline 2\nline 3');
     });
+
+    it("inserts a string to target after the last line as a new line", function() {
+      var target = 'line 1\nline 2';
+      var result = editer.insert('line 3', target, {after: {line: 2}, asNewLine: true});
+
+      expect(result).to.equal('line 1\nline 2\nline 3');
+    });
   });
 
   describe("before.line", function() {
@@ -41,6 +48,13 @@ describe(".insert", function() {
     it("inserts a string to target before the line as a new line", function() {
       var target = 'line 0\nline 2\nline 3';
       var result = editer.insert('line 1', target, {before: {line: 2}, asNewLine: true});
+
+      expect(result).to.equal('line 0\nline 1\nline 2\nline 3');
+    });
+
+    it("inserts a string to target before the first line as a new line", function() {
+      var target = 'line 1\nline 2\nline 3';
+      var result = editer.insert('line 0', target, {before: {line: 1}, asNewLine: true});
 
       expect(result).to.equal('line 0\nline 1\nline 2\nline 3');
     });
